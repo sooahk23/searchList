c6478362904fc87d2e7cfa1d1264fad28ca31f08
김수아 <sooahk@lguplus.co.kr>
2024-03-08 09:31:52 +0900

    Remove: non need code

--- app/src/main/java/com/example/daumsearch/viewmodel/WebViewModel.kt
+++ app/src/main/java/com/example/daumsearch/viewmodel/WebViewModel.kt
@@ -44,10 +44,6 @@ class WebViewModel (application: Application) : AndroidViewModel(application){
     var queryBefore = "" // 이전 검색어
     var docsPage = 1 // 웹문서 불러온 페이지 수
     var imgsPage = 1 // 이미지 불러온 페이지 수
-    lateinit  var docsRemain : List<ResponseWebMedium> // 남겨진 정제되지 않은 웹문서 리스트
-    lateinit var imgsRemain : List<ResponseWebMedium> // 남겨진 정제되지 않은 이미지 리스트
-    var docsIsLast = false // 마지막 웹문서 페이지인지
-    var imgsIsLast = false // 마지막 이미지 페이지인지
 
     private val db = DaumSearchApp.getDatabase(application)
     private val bookmarkDao: BookmarkDao = db.bookmarkDao()
@@ -56,8 +52,6 @@ class WebViewModel (application: Application) : AndroidViewModel(application){
         viewModelScope.launch {
             withContext(Dispatchers.IO){
                 try {
-//                    if (query != null && query != "") { // 처음 불러올 때
-//                        Log.d(TAG, "query is not null")
                         val docsDeferred = async { RetrofitClient.daumApiService.getDocuments(query ?: queryBefore, "recency", docsPage) }
                         val imgsDeferred = async { RetrofitClient.daumApiService.getImages(query?: queryBefore, "recency", imgsPage) }
 
@@ -65,21 +59,8 @@ class WebViewModel (application: Application) : AndroidViewModel(application){
                         val imgsRes = imgsDeferred.await()
 
                         if (docsRes.isSuccessful && imgsRes.isSuccessful) {
-                            var docs:List<ResponseWebMedium> = docsRes.body()?.documents ?: emptyList()
-                            var imgs:List<ResponseWebMedium> = imgsRes.body()?.documents ?: emptyList()
-
-                            docsIsLast = docsRes.body()?.meta?.is_end ?: false
-                            imgsIsLast = imgsRes.body()?.meta?.is_end ?: false
-
-//                            // TODO: !! 연산자를 사용하지 않도록 수정
-//                            val lastDateTime = if (docs.last().datetime!! > imgs.last().datetime!!) docs.last().datetime else imgs.last().datetime
-//
-//                            docsRemain = docs.filter { item -> item.datetime!! < lastDateTime }
-//                            docs = docs.filter { item -> item.datetime!! >= lastDateTime }
-//
-//                            imgsRemain = imgs.filter { item -> item.datetime!! < lastDateTime }
-//                            imgs = imgs.filter { item -> item.datetime!! >= lastDateTime }
-
+                            val docs:List<ResponseWebMedium> = docsRes.body()?.documents ?: emptyList()
+                            val imgs:List<ResponseWebMedium> = imgsRes.body()?.documents ?: emptyList()
 
                             var combinedList: List<ResponseWebMedium> = docs + imgs
                             combinedList = combinedList.sortedByDescending { item -> item.datetime }
@@ -108,45 +89,6 @@ class WebViewModel (application: Application) : AndroidViewModel(application){
                         } else {
                             Log.d(TAG, "response accepted but failed")
                         }
-
-//                    }
-//                    else if (query == null) { // 더 불러올 때
-//                         Log.d(TAG, "query is null")
-//                        // 이미 불러온 적이 있는 경우
-//                        if (docsRemain.isEmpty()) {
-//                            if (!docsIsLast){
-//                                // docs 불러오기
-//                                Log.d(TAG, "docs 불러오기 1")
-////                                val docsDeferred = async { RetrofitClient.daumApiService.getDocuments(queryBefore, "recency", docsPage) }
-////                                val docsRes = docsDeferred.await()
-////
-////                                if (docsRes.isSuccessful){
-////                                    var docs:List<ResponseWebMedium> = docsRes.body()?.documents ?: emptyList()
-////                                    docsIsLast = docsRes.body()?.meta?.is_end ?: false
-////                                }
-//                            }else if (!imgsIsLast){
-//                                // imgs 불러오기
-//                                Log.d(TAG, "imgs 불러오기 1")
-//                            }else{
-//                                Log.d(TAG, "All data is loaded")
-//                            }
-//                        }else if (imgsRemain.isEmpty()) {
-//                            if (!imgsIsLast){
-//                                // imgs 불러오기
-//                                Log.d(TAG, "imgs 불러오기 2")
-//                            }else if (!docsIsLast){
-//                                // docs 불러오기
-//                                Log.d(TAG, "docs 불러오기 2")
-//                            }else{
-//                                Log.d(TAG, "All data is loaded")
-//                            }
-//                        } else {
-//                            Log.d(TAG, "Something went wrong")
-//                        }
-//
-//                        // docsRemain, imgsRemain에 남아있는 데이터 합치기 진행
-//
-//                    }
                 } catch (e: Exception) {
                     // 예외 처리, 예를 들면 네트워크 오류 등
                     Log.d(TAG, "response failed in fetchDocs")
